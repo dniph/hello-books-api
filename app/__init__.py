2be1087a6e794cf7c6bb7bd839ec2be428a8c2aa
Dennif <dennif.pa@gmail.com>
2025-04-17 18:22:35 -0700

    get one book added

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,8 @@ from .routes.book_routes import books_bp
 
 def create_app():
     app = Flask(__name__)
-
+    
     # Register Blueprints here
     app.register_blueprint(books_bp)
-    
 
     return app
--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -3,6 +3,7 @@ from app.models.book import books
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+#Get all the books
 @books_bp.get("")
 def get_all_books():
     books_response = []
@@ -14,3 +15,15 @@ def get_all_books():
         ))
         
     return books_response
+
+#Get one book
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return dict(id = book.id,
+            title = book.title,
+            description = book.description
+                
+            )
